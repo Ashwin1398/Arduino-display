1f9c27bb5323db2445e1988745ab145af9acc16a
Ashwin_13 <33787005+Ashwin1398@users.noreply.github.com>
2019-02-04 20:24:54 +0530

    Updated source code files

---
+++ Crystal_display_0-9999/Crystal_display_0-9999.ino
@@ -0,0 +1,26 @@
+// include the library code
+#include<LiquidCrystal.h> 
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(8, 13, 9, 4, 5, 6, 7);
+
+int i;
+
+void setup() 
+{ 
+// set up the LCD's number of columns and rows: 
+ lcd.begin(16,2);
+ lcd.setCursor(0,0);
+ // Print a message to the LCD.
+ lcd.print("Count");
+}
+
+void loop()
+{
+  for(i=0;i<9999;i++)//to count from 0-9999
+  {
+  lcd.setCursor(6,0);
+ lcd.print(i);
+ delay(50); 
+ }
+}
---
+++ TM  1637 (0-9999)/tm0-9999/tm0-9999.ino
@@ -0,0 +1,21 @@
+#include <TM1637Display.h>
+
+const int CLK = 2; //Set the CLK pin connection to the display
+const int DIO = 3; //Set the DIO pin connection to the display
+
+int i=0;
+
+TM1637Display display(CLK, DIO);  //set up the 4-Digit Display.
+
+void setup()
+{
+  display.setBrightness(0x0a);  //set the diplay to maximum brightness
+}
+
+
+void loop()
+{
+  for (i=0; i<9999; i++){
+    display.showNumberDec(i); //Display the Variable value;
+    delay(50); }
+}
